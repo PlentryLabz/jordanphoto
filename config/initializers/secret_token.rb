79c0065f8ee1507c3566c48da1bdbf418aaa9296
Shumilkin Gena <shumilkin.gena@yandex.ru>
2014-03-12 10:00:29 +0400

    init app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jordanphoto::Application.config.secret_token = 'dde47cf58e0c79f22621f45c874114e08a08e166bb599adb65409c34eed0ab79cf4443efc537202bf238d6792748931d2c5514fba017920e5e3b4ac4e0e7dd00'
